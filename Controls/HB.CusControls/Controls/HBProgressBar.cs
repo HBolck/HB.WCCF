c20ab050c0d8e0c03cdae139f801ca28409c1074
Ld <id573345441@outlook.com>
2020-12-06 22:01:31 +0800

    添加新控件

---
+++ Controls/HB.CusControls/Controls/HBProgressBar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HB.CusControls.Controls
+{
+    public class HBProgressBar : ProgressBar
+    {
+
+        #region 依赖属性
+        /// <summary>
+        /// 是否只读
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get { return (bool)GetValue(IsReadOnlyProperty); }
+            set { SetValue(IsReadOnlyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsReadOnly.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsReadOnlyProperty =
+            DependencyProperty.Register("IsReadOnly", typeof(bool), typeof(HBProgressBar), new PropertyMetadata(false));
+
+
+
+        #endregion
+    }
+}
